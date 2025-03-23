ad7a902487c904428b15ebf506423da1186c1724
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2025-03-23 07:19:11 +0100

    Migrate to AWS CDK from Serverless

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # lambda-rss
 Go AWS Lambda function for scraping sites not offering RSS feeds and publish them as RSS feeds.
+
+## Setup
+1. Start by [bootstraping your AWS environment](https://docs.aws.amazon.com/cdk/v2/guide/bootstrapping-env.html).
+2. [Setup a new policy](https://stackoverflow.com/questions/57118082/what-iam-permissions-are-needed-to-use-cdk-deploy/61102280#61102280) granting permission to assume CDK roles.
+3. Add the following GitHub Actions secrets: `AWS_ACCESS_KEY_ID`, `AWS_SECRET_ACCESS_KEY`, and `AWS_DEFAULT_REGION`.
+4. Profit.
+
+## Usage
+Each `.go`-file in `app/sites` contains the logic to extract content from a URL and expose it as an RSS feed. Each feed can then be accessed via https://my-function-url/?s=site-name.
---
+++ cdk.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type StackProps struct {
+	awscdk.StackProps
+}
+
+func NewLambdaRssStack(scope constructs.Construct, id string, props *StackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+
+	stack := awscdk.NewStack(scope, &id, &sprops)
+	function := awslambda.NewFunction(stack, jsii.String("LambdaRssFunction"), &awslambda.FunctionProps{
+		Architecture: awslambda.Architecture_ARM_64(),
+		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
+		Handler:      jsii.String("bootstrap"),
+		Code:         awslambda.Code_FromAsset(jsii.String("./app/build/"), nil),
+	})
+
+	functionUrl := function.AddFunctionUrl(&awslambda.FunctionUrlOptions{
+		AuthType: awslambda.FunctionUrlAuthType_NONE,
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("lambdaRssFunctionUrlOutput"), &awscdk.CfnOutputProps{
+		Value: functionUrl.Url(),
+	})
+
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewLambdaRssStack(app, "LambdaRssStack", &StackProps{})
+
+	app.Synth(nil)
+}
